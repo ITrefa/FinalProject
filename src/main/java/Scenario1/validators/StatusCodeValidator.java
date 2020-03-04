911147529f9ce48786f2774430870c15130449c3
ITrefa <katilaletina113@gmail.com>
2020-03-04 13:33:37 +0300

    add log to StatusCodeValidator

--- src/main/java/Scenario1/validators/StatusCodeValidator.java
+++ src/main/java/Scenario1/validators/StatusCodeValidator.java
@@ -1,14 +1,19 @@
 package Scenario1.validators;
 
+import Scenario1.listeneres.ListenerByteSize;
 import com.griddynamics.jagger.coordinator.NodeContext;
 import com.griddynamics.jagger.engine.e1.collector.ResponseValidator;
 import com.griddynamics.jagger.engine.e1.collector.ResponseValidatorProvider;
 import com.griddynamics.jagger.invoker.v2.JHttpEndpoint;
 import com.griddynamics.jagger.invoker.v2.JHttpQuery;
 import com.griddynamics.jagger.invoker.v2.JHttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 
 public class StatusCodeValidator implements ResponseValidatorProvider {
+
+    private static Logger log = LoggerFactory.getLogger(StatusCodeValidator.class);
 
     @Override
     public ResponseValidator<JHttpQuery, JHttpEndpoint, JHttpResponse> provide(String taskId, String sessionId, NodeContext kernelContext) {
@@ -20,7 +25,11 @@ public class StatusCodeValidator implements ResponseValidatorProvider {
 
             @Override
             public boolean validate(JHttpQuery jHttpQuery, JHttpEndpoint endpoint, JHttpResponse jHttpResponse, long l) {
-                return jHttpResponse.getStatus() == HttpStatus.OK;
+                if (jHttpResponse.getStatus() != HttpStatus.OK) {
+                    log.error("Invalid response code " + jHttpResponse.getStatus() + " for endpoint " + endpoint);
+                    return false;
+                }
+                return true;
             }
         };
     }
