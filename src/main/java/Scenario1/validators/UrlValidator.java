57b9d1be5a69c0db272d6e366827a5b5f0bd647f
ITrefa <katilaletina113@gmail.com>
2020-03-04 15:43:31 +0300

    add log to UrlValidator

--- src/main/java/Scenario1/validators/UrlValidator.java
+++ src/main/java/Scenario1/validators/UrlValidator.java
@@ -6,8 +6,14 @@ import com.griddynamics.jagger.engine.e1.collector.ResponseValidatorProvider;
 import com.griddynamics.jagger.invoker.v2.JHttpEndpoint;
 import com.griddynamics.jagger.invoker.v2.JHttpQuery;
 import com.griddynamics.jagger.invoker.v2.JHttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UrlValidator implements ResponseValidatorProvider {
+
+    private static Logger log = LoggerFactory.getLogger(UrlValidator.class);
+
+
     @Override
     public ResponseValidator<JHttpQuery, JHttpEndpoint, JHttpResponse> provide(String taskId, String sessionId, NodeContext kernelContext) {
         return new ResponseValidator<JHttpQuery, JHttpEndpoint, JHttpResponse>(taskId, sessionId, kernelContext) {
@@ -18,7 +24,11 @@ public class UrlValidator implements ResponseValidatorProvider {
 
             @Override
             public boolean validate(JHttpQuery jHttpQuery, JHttpEndpoint endpoint, JHttpResponse jHttpResponse, long l) {
-                return jHttpResponse.getBody().toString().contains("http://httpbin.org/get");
+                if (!jHttpResponse.getBody().toString().contains("http://httpbin.org/get")) {
+                    log.error("Invalid url"  + " for endpoint " + endpoint);
+                    return false;
+                }
+                return true;
             }
         };
     }
